03c39a2cf970c92c2d6b32a9f7fe37cd385f4081
Aidana <a19071997@mail.ru>
2024-12-10 01:44:28 +0500

    json items

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,17 +1,28 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/",
-    ]
+class MySpider(scrapy.Spider):
+    name = "myspider"
+
+    # Настройки для вывода данных в файл
+    custom_feed = {
+        "/spiders/items.json": {  # Абсолютный путь
+            "format": "json",
+            "indent": 4,  # Форматировать JSON с отступами
+        }
+    }
+
+    @classmethod
+    def update_settings(cls, settings):
+        super().update_settings(settings)
+        # Обновляем настройки FEEDS
+        feeds = settings.setdefault("FEEDS", {})
+        feeds.update(cls.custom_feed)
+
+    def start_requests(self):
+        # Пример запроса
+        yield scrapy.Request(url="http://example.com", callback=self.parse)
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
-            yield {
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
-            }
+        # Пример обработки ответа
+        yield {"title": response.xpath("//title/text()").get()}
